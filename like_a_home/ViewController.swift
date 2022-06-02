86ef9903e01880cf57bb9311e8c1c17153b6207a
Husoid <husoid@mail.ru>
2022-06-02 12:17:17 +0500

    init

---
+++ like_a_home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  like_a_home
+//
+//  Created by User on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
